dbc41bdb1adcd195a9572b936bc16b4d5b2b46b9
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-20 10:17:37 +0100

    Commit

---
+++ Termometar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Unesite temperaturu u Farenhajtima:");
+        double fahrenheit = double.Parse(Console.ReadLine());
+        double celsius = F2C(fahrenheit);
+
+        Console.WriteLine($"Temperatura u Celzijusima: {celsius}");
+    }
+
+    static double F2C(double f)
+    {
+        return (f - 32) * 5 / 9;
+    }
+}
